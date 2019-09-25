cf6387faf61e724e0c7d85a68bee0123648a0904
SegoviaJ <james.segovia@hotmail.com>
2019-09-25 14:00:49 -0700

    mer

--- Ch03/percent_discount.py
+++ Ch03/percent_discount.py
@@ -1,8 +1,8 @@
 import sys
 
-price = float(sys.argv[1])
-percent = float(sys.argv[2])
+price = int(sys.argv[1])
+percent = int(sys.argv[2])
 
 discount = str(round(price-percent/100*price,2))
-
+int
 print('$'+sys.argv[1]+' at '+sys.argv[2]+'% discount is $'+discount)
